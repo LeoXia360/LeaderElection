c5f400614fb3a7f02438b921fc10d0317fc18ad8
leoxia360 <leoxia360@gmail.com>
2021-06-15 19:05:36 -0700

    Initial commit

---
+++ src/handlers/FollowerHandler.java
@@ -0,0 +1,18 @@
+package handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class FollowerHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange t) throws IOException {
+        String response = "I am a follower";
+        t.sendResponseHeaders(200, response.length());
+        OutputStream os = t.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
